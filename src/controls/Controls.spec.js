5a3630e670112f956182609904768f22d6b8b945
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-10-14 21:02:36 -0400

    open and unlock

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+
+import React from 'react';
+import "react-testing-library/cleanup-after-each";
+import Controls from './Controls';
+import { render, fireEvent } from 'react-testing-library';
+
+describe("<Controls />", () => {
+    it("open and unlock", () => {
+        const mock = jest.fn();
+        const { queryByText } = render(<Controls locked={false} closed={false} toggleClosed={mock} />)
+        const lockButton = queryByText("Lock Gate");
+        expect(lockButton.disabled).toBe(true)
+        const closeButton = queryByText("Close Gate");
+        expect(closeButton.disabled).toBe(false)
+        fireEvent.click(closeButton);
+        expect(mock).toBeCalled()
+    })
+})
